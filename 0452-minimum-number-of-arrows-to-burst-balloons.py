5ad9cad3d4dfb8b592493448291583f6680dbfcc
El Hassane Boushib <boushib.elhassane@gmail.com>
2024-09-12 19:52:47 -0400

    Add 0452-minimum-number-of-arrows-to-burst-balloons

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -0,0 +1,21 @@
+from typing import List
+from tests import run_tests
+
+
+def find_min_arrow_shots(points: List[List[int]]) -> int:
+    res = 1
+    points.sort(key=lambda x: x[1])
+    next_end = points[0][1]
+
+    for a, b in points:
+        if a > next_end:
+            res += 1
+            next_end = b
+
+    return res
+
+
+tests = [([[10, 16], [2, 8], [1, 6], [7, 12]], 2),
+         ([[1, 2], [3, 4], [5, 6], [7, 8]], 4),
+         ([[1, 2], [2, 3], [3, 4], [4, 5]], 2)]
+run_tests(find_min_arrow_shots, tests)
