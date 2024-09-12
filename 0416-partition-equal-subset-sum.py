8890067089e05ddc0582949bf3c32e0586fd9c05
El Hassane Boushib <boushib.elhassane@gmail.com>
2024-09-12 16:08:22 -0400

    Add 0416-partition-equal-subset-sum

---
+++ 0416-partition-equal-subset-sum.py
@@ -0,0 +1,24 @@
+from typing import List
+from tests import run_tests
+
+
+def can_partition(nums: List[int]) -> bool:
+    if sum(nums) % 2 == 1:
+        return False
+
+    target = sum(nums) // 2
+    dp = set([0])
+
+    for n in nums:
+        # Should not change set size during iteration
+        next_dp = dp.copy()
+
+        for m in dp:
+            next_dp.add(n + m)
+
+        dp = next_dp
+    return target in dp
+
+
+tests = [([1, 5, 11, 5], True), ([1, 2, 3, 5], False)]
+run_tests(can_partition, tests)
