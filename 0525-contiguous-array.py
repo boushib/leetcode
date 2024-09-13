fd7e8eeb832ad78074198a05676285a838bef43a
El Hassane Boushib <boushib.elhassane@gmail.com>
2024-09-12 20:18:41 -0400

    Add 0525-contiguous-array

---
+++ 0525-contiguous-array.py
@@ -0,0 +1,22 @@
+from typing import List
+from tests import run_tests
+
+
+def find_max_length(nums: List[int]) -> int:
+    res = 0
+    count = 0
+    count_map = {0: -1}
+
+    for i, n in enumerate(nums):
+        count += 1 if n == 1 else -1
+
+        if count in count_map:
+            res = max(res, i - count_map[count])
+        else:
+            count_map[count] = i
+
+    return res
+
+
+tests = [([0, 1], 2), ([0, 1, 0], 2)]
+run_tests(find_max_length, tests)
