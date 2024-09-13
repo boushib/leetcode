8d6df37e5cee979b7e7564e9e2c10df919a0770c
El Hassane Boushib <boushib.elhassane@gmail.com>
2024-09-12 20:49:24 -0400

    Add 0824-goat-latin

---
+++ 0824-goat-latin.py
@@ -0,0 +1,25 @@
+from tests import run_tests
+
+
+def to_goat_latin(s: str) -> str:
+    volwels = set(["a", "e", "i", "o", "u"])
+    words = s.split(" ")
+
+    for i, w in enumerate(words):
+        if w[0].lower() not in volwels:
+            w = w[1:] + w[0]
+        words[i] = w + "ma" + "a" * (i + 1)
+
+    return " ".join(words)
+
+
+tests = [
+    ("I speak Goat Latin", "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"),
+    ("The quick brown fox jumped over the lazy dog",
+     "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa"
+     ),
+    ("Each word consists of lowercase and uppercase letters only",
+     "Eachmaa ordwmaaa onsistscmaaaa ofmaaaaa owercaselmaaaaaa andmaaaaaaa uppercasemaaaaaaaa etterslmaaaaaaaaa onlymaaaaaaaaaa"
+     )
+]
+run_tests(to_goat_latin, tests)
