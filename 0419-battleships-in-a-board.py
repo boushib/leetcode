c01a2f84c607dcf3637b59202149dd4292aa174a
El Hassane Boushib <boushib.elhassane@gmail.com>
2024-09-12 21:53:42 -0400

    Add 0419-battleships-in-a-board

---
+++ 0419-battleships-in-a-board.py
@@ -0,0 +1,25 @@
+from typing import List
+from tests import run_tests
+
+
+def count_battleships(board: List[List[str]]) -> int:
+    res = 0
+    rows, cols = len(board), len(board[0])
+
+    def is_part_of_battleship(i, j):
+        return i >= 0 and j >= 0 and board[i][j] == "X"
+
+    for i in range(rows):
+        for j in range(cols):
+            if is_part_of_battleship(i, j) and not is_part_of_battleship(
+                    i - 1, j) and not is_part_of_battleship(i, j - 1):
+                res += 1
+
+    return res
+
+
+tests = [
+    ([["X", ".", ".", "X"], [".", ".", ".", "X"], [".", ".", ".", "X"]], 2),
+    ([["."]], 0),
+]
+run_tests(count_battleships, tests)
