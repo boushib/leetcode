d3aa2c49195a62584793a519773bfb0b44292d49
El Hassane Boushib <boushib.elhassane@gmail.com>
2024-09-12 20:46:53 -0400

    Add 0560-subarray-sum-equals-k

---
+++ 0560-subarray-sum-equals-k.py
@@ -0,0 +1,19 @@
+from typing import List
+from tests import run_tests
+
+
+def subarray_sum(nums: List[int], k: int) -> int:
+    res = 0
+    cum_sum = 0
+    prefix_sum = {0: 1}
+
+    for n in nums:
+        cum_sum += n
+        res += prefix_sum.get(cum_sum - k, 0)
+        prefix_sum[cum_sum] = prefix_sum.get(cum_sum, 0) + 1
+
+    return res
+
+
+tests = [(([1, -1, 1, 1, 1], 2), 4), (([1, 1, 1], 2), 2), (([1, 2, 3], 3), 2)]
+run_tests(subarray_sum, tests)
