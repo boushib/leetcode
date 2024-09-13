cc89dd71af2d24ccade481221c1d7a36d2c2570b
El Hassane Boushib <boushib.elhassane@gmail.com>
2024-09-12 20:57:47 -0400

    Add 1752-check-if-array-is-sorted-and-rotated

---
+++ 1752-check-if-array-is-sorted-and-rotated.py
@@ -0,0 +1,20 @@
+from typing import List
+from tests import run_tests
+
+
+def is_sorted_and_rotated(nums: List[int]) -> bool:
+    n = len(nums)
+    count = 0
+
+    for i in range(n):
+        if nums[i] > nums[(i + 1) % n]:
+            count += 1
+
+        if count > 1:
+            return False
+
+    return True
+
+
+tests = [([3, 4, 5, 1, 2], True), ([2, 1, 3, 4], False), ([1, 2, 3], True)]
+run_tests(is_sorted_and_rotated, tests)
