8eb9d3b8a03edd3ffdaae176bec1e569079a5c2c
El Hassane Boushib <boushib.elhassane@gmail.com>
2024-09-13 16:16:56 -0400

    Add 0242-valid-anagram

---
+++ 0242-valid-anagram.py
@@ -0,0 +1,21 @@
+from tests import run_tests
+
+
+def is_anagram(s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
+    count_s, count_t = {}, {}
+
+    for i in range(len(s)):
+        count_s[s[i]] = count_s.get(s[i], 0) + 1
+        count_t[t[i]] = count_t.get(t[i], 0) + 1
+
+    for k, v in count_s.items():
+        if v != count_t.get(k, 0):
+            return False
+
+    return True
+
+
+tests = [(("anagram", "nagaram"), True), (("rat", "car"), False)]
+run_tests(is_anagram, tests)
