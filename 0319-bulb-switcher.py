f18defb4faa5ab5782bc914931bbec41c7506b79
El Hassane Boushib <boushib.elhassane@gmail.com>
2024-09-12 21:46:50 -0400

    Add 0319-bulb-switcher

---
+++ 0319-bulb-switcher.py
@@ -0,0 +1,14 @@
+from math import sqrt
+from tests import run_tests
+
+
+def bulb_switch(n: int) -> int:
+    return int(sqrt(n))
+
+
+tests = [
+    (3, 1),
+    (0, 0),
+    (1, 1),
+]
+run_tests(bulb_switch, tests)
