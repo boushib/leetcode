58bde9d0b6094a74665764c0ec56e9a8ceefe00d
El Hassane Boushib <boushib.elhassane@gmail.com>
2024-09-13 00:08:34 -0400

    Add 0896-monotonic-array.py

---
+++ 0896-monotonic-array.py
@@ -0,0 +1,21 @@
+from typing import List
+from tests import run_tests
+
+
+def is_monotonic(nums: List[int]) -> bool:
+    is_increasing, is_decreasing = False, False
+
+    for i in range(1, len(nums)):
+        if nums[i] > nums[i - 1]:
+            is_increasing = True
+        if nums[i] < nums[i - 1]:
+            is_decreasing = True
+
+        if is_increasing and is_decreasing:
+            return False
+
+    return True
+
+
+tests = [([1, 2, 2, 3], True), ([6, 5, 4, 4], True), ([1, 3, 2], False)]
+run_tests(is_monotonic, tests)
