726ba6408dda4d77d8fe452cfc56d429047cbb71
El Hassane Boushib <boushib.elhassane@gmail.com>
2024-09-12 21:42:53 -0400

    Add 1309-decrypt-string-from-alphabet-to-integer-mapping

---
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping.py
@@ -0,0 +1,22 @@
+from tests import run_tests
+
+
+def freq_alphabets(s: str) -> str:
+    res = []
+    n = len(s)
+    alphabets = "-abcdefghijklmnopqrstuvwxyz"
+    i = 0
+
+    while i < n:
+        if i < n - 2 and s[i + 2] == "#":
+            res.append(alphabets[int(s[i] + s[i + 1])])
+            i += 3
+        else:
+            res.append(alphabets[int(s[i])])
+            i += 1
+
+    return "".join(res)
+
+
+tests = [("10#11#12", "jkab"), ("1326#", "acz")]
+run_tests(freq_alphabets, tests)
