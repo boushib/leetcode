0335a6dbd700a4b50bb61908c22a1092e5387e84
El Hassane Boushib <boushib.elhassane@gmail.com>
2024-09-12 20:55:41 -0400

    Add 2595-number-of-even-and-odd-bits

---
+++ 2595-number-of-even-and-odd-bits.py
@@ -0,0 +1,18 @@
+from typing import List
+from tests import run_tests
+
+
+def even_odd_bit(n: int) -> List[int]:
+    res = [0, 0]
+
+    for i, c in enumerate(bin(n)[2:][::-1]):
+        if c == "1" and i % 2 == 0:
+            res[0] += 1
+        if c == "1" and i % 2 == 1:
+            res[1] += 1
+
+    return res
+
+
+tests = [(50, [1, 2]), (2, [0, 1])]
+run_tests(even_odd_bit, tests)
